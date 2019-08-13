a768e0eb07f7e75191fec377088e88aa6729cc3d
Link Qian <link.com@yeah.net>
2019-08-13 23:50:12 +0800

    Add files via upload
    upload demo

--- src/examples/demo.cpp
+++ src/examples/demo.cpp
@@ -16,17 +16,15 @@ using namespace cv;
 int main(int argc, char ** argv) {
 	MLPDR detector(argv[1], 0.9f, 0.8f, 0.8f);
 	Mat img = imread(argv[2]);
-	int minFaceSize = 40;
 	TickMeter tm;
 	tm.start();
 //	std::vector<mlpdr::FaceInfo> faceInfos = detector.Detect(img, minFaceSize, 3);  // 608.23 ms
-	vector<string> plateNos;
-	detector.recognizePlateNos(img, plateNos);
+	vector<PlateInfo> plateInfos = detector.recognize(img);
 	tm.stop();
 	printf("detect cost: %f (ms)\n", tm.getTimeMilli());
 
-	for (auto plateNo: plateNos) {
-		cout << "plateNo: " << plateNo << endl;
+	for (auto pi: plateInfos) {
+		cout << "plateNo: " << pi.plateNo << endl;
 	}
 
 }
